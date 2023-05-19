6beed6e8b2cee85ed8ef8d1cad49202dfde76403
georgianjagjit <jagjit4296@gmail.com>
2023-05-19 13:41:55 -0600

    solved session 2 more que

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -1,4 +1,3 @@
-//LCM of two numbers
 
 import java.lang.*;
 import java.util.*;
@@ -7,20 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int num_A = scn.nextInt();
-        int num_B = scn.nextInt();
-
-        int HCF = 1;
-        int LCM = 1;
-
-        int min_number = Math.min(num_A, num_B); 
         
-        for(int i=1; i<=min_number; i++){
-            if(num_A%i==0 && num_B%i==0){
-                HCF = i;
+        for(int i = 1; i <= num_A; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
             }
+            System.out.println();
         }
-
-        LCM = (num_A*num_B)/HCF;
-        System.out.println(LCM);
     }
 }
