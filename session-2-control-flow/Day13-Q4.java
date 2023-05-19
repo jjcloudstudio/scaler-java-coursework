5f8802ffe05297cfc664a3d01dc11b3b9c52c900
georgianjagjit <jagjit4296@gmail.com>
2023-05-19 14:11:48 -0600

    solved session 2 more que

--- session-2-control-flow/Day13-Q4.java
+++ session-2-control-flow/Day13-Q4.java
@@ -1,13 +1,13 @@
-import java.lang.*;
-import java.util.*;
+// import java.lang.*;
+// import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int naturalNum = scn.nextInt();
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         int naturalNum = scn.nextInt();
 
-        for(int i=2; i<=naturalNum; i++) {
-            System.out.print(i+ " ");
-        }
-    }
-}
+//         for(int i=2; i<=naturalNum; i++) {
+//             System.out.print(i+ " ");
+//         }
+//     }
+// }
--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -1,17 +1,34 @@
-
 import java.lang.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+
+    static boolean isPerfectSquare(int N){
+        int i = 1;
+        while(i*i <= N){
+            if(i*i == N){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+    static int sumEven(int N){
+        int sum = 0;
+        for(int i = 2; i <= N; i=i+2){
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    static int takeInput(){
         Scanner scn = new Scanner(System.in);
         int num_A = scn.nextInt();
-        
-        for(int i = 1; i <= num_A; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        return num_A;
+    }
+    public static void main(String[] args) {
+        int num_A = takeInput();
+        boolean finalAns = isPerfectSquare(num_A);
+        System.out.println(finalAns);
     }
 }
