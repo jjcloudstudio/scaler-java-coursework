762ef39d839548414e4dfbce694a9f732712d9c6
georgianjagjit <jagjit4296@gmail.com>
2023-05-20 09:54:11 -0600

    solved session 2 more que

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -3,51 +3,24 @@ import java.util.*;
 
 public class Main {
 
-    static void printPattern(int num_A) {
-        for (int i = 1; i <= num_A; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            //print spaces in between
-            if (i != num_A) {
-                for (int j = 1; j <= num_A - i; j++) {
-                    System.out.print("  ");
-                }
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+    static void printFactorial(int num_A) {
+        int factorial = 1;
+        if (num_A == 0) {
+            System.out.println("1");
+            return;
         }
-        
+        for (int i = 1; i <= num_A; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
     }
-
-
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int num_A = scn.nextInt();
         int temp_num_A = num_A;
 
-        printPattern(num_A);
+        printFactorial(num_A);
 
-        // for(int i = num_A; i >= 1; i--){
-        // for(int j = 1; j <= i; j++){
-        // System.out.print("*");
-        // }
-        // if(num_A == temp_num_A){
-        // for(int j = 1; j <= temp_num_A; j++){
-        // System.out.print("*");
-        // }
-        // temp_num_A--;
-        // }
-        // else{
-        // for(int j = 1; j <= temp_num_A; j++){
-        // System.out.print(" ");
-        // }
-        // temp_num_A--;
-        // }
-        // System.out.println();
-        // }
     }
 }
