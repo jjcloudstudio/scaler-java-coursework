fe4a8c2510ffc1eda7cb4178964d906b6c272ff0
georgianjagjit <jagjit4296@gmail.com>
2023-05-23 12:37:05 -0600

    print pyramid

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -6,13 +6,19 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
         int counter = 1;
+
         for(int i=1; i<=num; i++){
+            //task for i-th row
+            for(int j=1; j<=num-i; j++){ // n+m = i+1 => m = i+1-n
+                System.out.print("_");
+            }
+
             for(int j=1; j<=i; j++){
-                System.out.print(counter + " ");
-                counter++;
+                System.out.print(j + "_");
             }
             System.out.println();
         }
+
     }
 }
 
