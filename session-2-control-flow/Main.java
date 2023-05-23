44986bbb1d5a88dbf64831a86732744b3eeafff5
georgianjagjit <jagjit4296@gmail.com>
2023-05-23 12:51:22 -0600

    sum of sq of all values

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -1,29 +1,24 @@
-//____1_
-// ___1_2_
-// __1_2_3_
-// _1_2_3_4_
-// 1_2_3_4_5_
-
+// 1
+// 2
+// 3
+// 5
+// 6
+// 75
 import java.lang.*;
 import java.util.*;
 
 public class Main {    
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int num = scn.nextInt();
-        int counter = 1;
+        
+        int sum = 0;
 
-        for(int i=1; i<=num; i++){
-            //task for i-th row
-            for(int j=1; j<=num-i; j++){ // i + spaces = num => spaces = num-i 
-                System.out.print("_");
-            }
+        for(int i=1; i<=5; i++){
+            Scanner scn = new Scanner(System.in);
+            int num = scn.nextInt();
 
-            for(int j=1; j<=i; j++){
-                System.out.print(j + "_");
-            }
-            System.out.println();
+            sum = sum + num*num;
         }
+        System.out.println(sum);
 
     }
 }
