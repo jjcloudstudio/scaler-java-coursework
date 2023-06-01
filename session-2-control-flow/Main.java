fcd65cc9c214d72c4fcfe114a8272cc55e690760
georgianjagjit <jagjit4296@gmail.com>
2023-05-31 22:25:59 -0600

    max minus min temp

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
 public class Main {    
-    static void print2DArrayWaveForm(int mat[][]){
-        for(int i=0;i<mat.length;i++){
-            if(i%2==0){
-                for(int j=0;j<mat[i].length;j++){
-                    System.out.print(mat[i][j]+" ");
-                }
-            }else{
-                for(int j=mat[i].length-1;j>=0;j--){
-                    System.out.print(mat[i][j]+" ");
-                }
-            }
-        }
+    static int countEvenMinusOddNum(int[] A){
+        
     }
 
-    static int[][] takeInput(){
+    static int[] takeInput(){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] mat = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                mat[i][j] = sc.nextInt();
+        int total_cases = sc.nextInt();
+        for(int i=0; i<total_cases; i++){
+            int size = sc.nextInt();
+            int[] A = new int[size];
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for(int j=0; j<size; j++){
+                A[j] = sc.nextInt();
+                if(A[j]%2==0){
+                    evenCount++;
+                }else{
+                    oddCount++;
+                }
             }
+
+
         }
-        return mat;
     }
 
     public static void main(String[] args) {
-        int[][] mat = takeInput();
-        print2DArrayWaveForm(mat);
+        //que: how to take whole array as input
+        
+        
+        int[] A = takeInput();
+        System.out.println(countEvenMinusOddNum(A));
     }
 } 
