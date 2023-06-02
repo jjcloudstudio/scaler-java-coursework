b1ca722557c5bab275fa2bef1b266652171efae9
georgianjagjit <jagjit4296@gmail.com>
2023-06-01 20:44:23 -0600

    pair count

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -1,53 +1,40 @@
-// Importing the java.util package which contains Scanner class
 import java.util.*;
 
-// Defining a public class named Main
 public class Main {    
     
-    // Defining a static method takeArraySize which takes an object of Scanner as input and returns an integer value.
     static int takeArraySize(Scanner sc){
-        // Reading an integer from standard input using Scanner object and storing it in total_cases variable.
         int total_cases = sc.nextInt();
-        return total_cases; // Returning the read integer value.
+        return total_cases;
     }
 
-    // Defining another static method takeArray which takes two inputs - an object of Scanner and an integer value, 
-    // and returns an array of integers.
     static int[] takeArray(Scanner sc, int total_cases){
-        // Creating a new array of integers with size equal to the given 'total_cases' parameter.
         int[] arr = new int[total_cases];
-        
         for(int i=0; i<total_cases; i++){
-            arr[i] = sc.nextInt();  // Reading integers from standard input using Scanner object and storing them in array elements one by one.
+            arr[i] = sc.nextInt();
         }
-        
-        return arr;  // Returning the created array containing all read integers.
+        return arr;
     }
 
-      // Defining another static method getNegativeNum which takes an array of integers as input,
-      // counts negative numbers present in it, prints them on console output stream,
-      // but always returns zero (0).
-      
-    static int getNegativeNum(int[] A){
-          int count = 0;
-      
-          for(int i=0; i<A.length; i++){
-              if(A[i]<0){  
-                  System.out.print(A[i]);  // Printing negative number found at index 'i' on console output stream without any newline character. 
+    static int[] NumOfElementGreaterThanItself(int[] A){
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<A.length; i++){
+            max = Math.max(max, A[i]);
+        }
+        int count = 0;
+        for(int i=0; i<A.length; i++){
+            if(A[i] < max){
+                count++;
             }
         }
-      
-          return 0; // Always returning zero (0) from this method.
+        return count;
     }
     
-      // Defining the main method which is the entry point of our program.
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  // Creating a new object of Scanner class to read input from standard input stream.
-        
-        int total_cases = takeArraySize(sc);  // Calling takeArraySize method with scanner object as parameter and storing its returned value in 'total_cases' variable.
-
-        int[] arr = takeArray(sc, total_cases);  // Calling takeArray method with scanner object and 'total_cases' variable as parameters, and storing its returned array in 'arr' variable.
-
-        int countOfNegativeNum = getNegativeNum(arr);  // Calling getNegativeNum method with 'arr' array as parameter and storing its returned integer value in 'countOfNegativeNum' variable. Note that this value will always be zero (0).
+        Scanner sc = new Scanner(System.in);
+        int total_cases = takeArraySize(sc);
+        int[] arr = takeArray(sc, total_cases);
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int[] reverse_PartOf_Array = NumOfElementGreaterThanItself(arr);
     }
-}
+} 
