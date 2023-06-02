17117cd52db856d0927657fa7b652eb711c3d13a
georgianjagjit <jagjit4296@gmail.com>
2023-06-02 15:34:51 -0600

    all odd number from dynamic array

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -1,42 +1,41 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
-public class Main {    
-    
-    static int takeArraySize(Scanner sc){
-        int total_cases = sc.nextInt();
-        return total_cases;
-    }
+public class Main {
 
-    static int[] takeArray(Scanner sc, int total_cases){
-        int[] arr = new int[total_cases];
-        for(int i=0; i<total_cases; i++){
-            arr[i] = sc.nextInt();
+    static int[][] takeArray(Scanner sc, int rows, int columns){
+        int[][] arr = new int[rows][columns];
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                arr[i][j] = sc.nextInt();
+            }
         }
         return arr;
     }
 
-    static int[] getEvenNumbers(int[] A){
-        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
-        int j = 0;
-        for(int i=0; i<A.length; i++){
-            if(A[i]%2 == 0){
-                evenNumbers.add(A[i]);
+    static int[][] addTwoMatrix(int[][] A, int[][]B){
+        int n=A.length;
+        int m=A[0].length;
+        int[][] C = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                C[i][j] = A[i][j] + B[i][j];
             }
         }
-        int[] result = new int[evenNumbers.size()];
-        for(int i=0; i<evenNumbers.size(); i++){
-            result[i] = evenNumbers.get(i);
-        }
-        return result;
+        return C;
     }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int total_cases = takeArraySize(sc);
-        int[] arr = takeArray(sc, total_cases);
-        int[] evenNumbers = getEvenNumbers(arr);
-        System.out.println(Arrays.toString(evenNumbers));
-        
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        int[][] arr1 = takeArray(sc, rows, columns);
+        int[][] arr2 = takeArray(sc, rows, columns);
+        int[][] addedThirdMatrix = addTwoMatrix(arr1, arr2);
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                System.out.print(addedThirdMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-} 
+}
