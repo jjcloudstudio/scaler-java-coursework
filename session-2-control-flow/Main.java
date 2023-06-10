8d32b38050331cdfc8f8c66fa7ca0a5a129fdadd
georgianjagjit <jagjit4296@gmail.com>
2023-06-09 21:01:25 -0600

    solve

--- session-2-control-flow/Main.java
+++ session-2-control-flow/Main.java
@@ -14,49 +14,55 @@ public class Main {
         }
         return arr;
     }
-
-    static int[][] takeArray2D(Scanner sc, int total_cases) {
-        int[][] arr = new int[total_cases][total_cases];
-        for (int i = 0; i < total_cases; i++) {
-            for (int j = 0; j < total_cases; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        return arr;
+    //take string input from user
+    static String takeString(Scanner sc) {
+        String str = sc.nextLine();
+        return str;
     }
 
-    public void rotateMatrix90degreeClockwise(int[][] A) {
-        // First, we perform the matrix transposition.
-        // A.length gives us the size of the matrix.
-        // Transposition is achieved by swapping elements at position (i,j) with elements at position (j,i)
-        // We only iterate up to j=i to avoid swapping elements back to their original position
-        for(int i=0; i<A.length; i++){
-            for(int j=i; j<A[i].length; j++){
-                int temp = A[i][j];
-                A[i][j] = A[j][i];
-                A[j][i] = temp;
-            }
+    //given string, count number of uppercase and lowercase characters without using built-in functions condition should be 'A' 'Z'
+    static void countUpperLower(String str) {
+        int upper = 0, lower = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+                upper++;
+            else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+                lower++;
         }
-
-        // After transposition, we reverse each row to achieve the final rotated matrix.
-        // The inner loop only goes up to A.length/2 because we're swapping elements across the midpoint
-        for(int i=0; i<A.length; i++){
-            for(int j=0; j<A[i].length/2; j++){
-                int temp = A[i][j];
-                A[i][j] = A[i][A[i].length-1-j];
-                A[i][A[i].length-1-j] = temp;
-            }
-        }
+        System.out.println("Upper: " + upper + " Lower: " + lower);
     }
+    
+    
+    public static ArrayList<ArrayList<Integer>> solve(ArrayList<ArrayList<Integer>> A) {
+        int n = A.size();
+        int m = A.get(0).size();
+    
+        ArrayList<ArrayList<Integer>>ans = new ArrayList<>();
+    
+        for(int i=0; i < n;i++) {
+            ArrayList<Integer>temp = new ArrayList<>();
+            for(int j=0; j < m;j++) {
+                if(A.get(i).get(j) % 2 == 0) {
+                    temp.add(A.get(i).get(j));
+                }
+            }
+        
+            ans.add(temp);
+        }
+    
+        return ans;
+    }
+
+    // int arr[][] = new int[N][M];
+    // ArrayList<Interger> al = new ArrayList<>();
+    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int total_cases = takeArraySize(sc);
         int[] arr1 = takeArray(sc, total_cases);
-        int[][] arr2 = takeArray2D(sc, total_cases);
-        int[] ansMatrix = UniqueElements(arr1);
-        System.out.println(Arrays.toString(ansMatrix)); 
+        int[][] arr2 = takeArray2D(sc, total_cases); 
 
-        rotateMatrix90degreeClockwise(arr2, arr2);
+        allDiagonalSum(arr2, arr2);
     }
 }
